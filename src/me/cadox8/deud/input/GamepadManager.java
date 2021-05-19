bed756b2c031a56b333c5045d1499b877a15f6e5
Cadox8 <cadox8@gmail.com>
2021-05-19 14:28:41 +0200

    New library for controller

--- src/me/cadox8/deud/input/GamepadManager.java
+++ src/me/cadox8/deud/input/GamepadManager.java
@@ -1,6 +1,7 @@
 package me.cadox8.deud.input;
 
-// ToDo: This class will provide support for Xbox and PlayStation controllers using ????
+// ToDo: This class will provide support for Xbox and PlayStation controllers using https://github.com/jessepav/sdljoystick4java
+// WARNING: This will only work for Windows...
 public class GamepadManager {
 
 }
